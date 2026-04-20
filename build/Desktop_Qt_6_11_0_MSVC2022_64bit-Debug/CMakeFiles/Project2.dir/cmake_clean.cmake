f22342e90adc43df733d6c8ce6db2d53415dd05c
Camila Pluas <camilapluas@usf.edu>
2026-04-19 20:54:15 -0400

    Clean up build files and ignore local configs

---
+++ build/Desktop_Qt_6_11_0_MSVC2022_64bit-Debug/CMakeFiles/Project2.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Project2_autogen.dir/ParseCache.txt"
+  "Project2_autogen"
+  "CMakeFiles/Project2.dir/Project2_autogen/EWIEGA46WW/qrc_resources.cpp.obj"
+  "CMakeFiles/Project2.dir/Project2_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d"
+  "CMakeFiles/Project2.dir/Project2_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/Project2.dir/Project2_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/Project2.dir/loginwindow.cpp.obj"
+  "CMakeFiles/Project2.dir/loginwindow.cpp.obj.d"
+  "CMakeFiles/Project2.dir/main.cpp.obj"
+  "CMakeFiles/Project2.dir/main.cpp.obj.d"
+  "CMakeFiles/Project2.dir/mainwindow.cpp.obj"
+  "CMakeFiles/Project2.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/Project2.dir/studentdisplay.cpp.obj"
+  "CMakeFiles/Project2.dir/studentdisplay.cpp.obj.d"
+  "Project2.exe"
+  "Project2.exe.manifest"
+  "Project2.lib"
+  "Project2.pdb"
+  "Project2_autogen/EWIEGA46WW/qrc_resources.cpp"
+  "Project2_autogen/mocs_compilation.cpp"
+  "Project2_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
